bbf0dddb7b2de9252a8d1fecba66a0cc842c74d8
khairunnazz-cell <khairunnazz@gmail.com>
2026-09-25 10:03:30 +0700

    css update

--- js/theme.js
+++ js/theme.js
@@ -6,6 +6,13 @@
 (function () {
     var LS_KEY = 'siec_theme';
     var VALID = ['default', 'midnight', 'emerald', 'sunset', 'royal'];
+    var THEME_COLORS = {
+        'default': '#2563eb',
+        'midnight': '#0b1220',
+        'emerald': '#059669',
+        'sunset': '#ea580c',
+        'royal': '#6d28d9'
+    };
 
     function applyTheme(t) {
         if (VALID.indexOf(t) === -1) t = 'default';
@@ -13,6 +20,9 @@
             document.documentElement.setAttribute('data-theme', t);
         }
         try { localStorage.setItem(LS_KEY, t); } catch (e) {}
+        // Sinkronkan warna status bar HP (meta theme-color) dengan tema aktif
+        var m = document.querySelector('meta[name="theme-color"]');
+        if (m && THEME_COLORS[t]) m.setAttribute('content', THEME_COLORS[t]);
     }
 
     async function syncFromServer() {
